6ff75bff23c7303afcefbf7ae2aa0f7f2d07daa6
Edward Dal Santo <edalsanto@doximity.com>
2018-05-21 21:53:39 -0700

    Start mult.asm

--- projects/04/mult/mult.asm
+++ projects/04/mult/mult.asm
@@ -6,4 +6,24 @@
 // Multiplies R0 and R1 and stores the result in R2.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
-// Put your code here.
+// High-Level Algorithm
+// load register to 0 for sum
+// loop R1 times and add R0 to sum each time
+// set R2 to sum either at end or throughout?
+
+// Initialize sum
+@0   // load 0 into A register
+D=A  // set D to value of A address (0)
+@sum // some register which will hold sum
+M=D  // set value at this register to D (0)
+
+// Initialize i for iteration
+@1   // load 1 into A Register
+D=A  // set D to value of A address (1)
+@i   // sum register which hold i
+M=D  // set value to to register D (1)
+
+(LOOP)
+  @i   // load register i
+  D=M // set D to value at i
+(END)
