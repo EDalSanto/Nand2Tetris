0092d81736d72ac5f214a3ac1ad4887df8f26c72
Edward Dal Santo <edalsanto@doximity.com>
2018-06-01 09:10:42 -0700

    re-arrange assembler

--- projects/06/Hack.py
+++ projects/06/Hack.py
@@ -16,7 +16,7 @@
         #   set binary output string to decimal convert to binary
         # else C command
         #   Code Module */
-        #     define structs
+        #     define hash table
         #       dest
         #       comp
         #         note: multiple keys with same value
@@ -30,34 +30,58 @@
 #!/usr/local/bin/python
 import sys
 
-class Parser():
+class HackAssembler():
+    """
+    Specific to Hack Computer in Nand2Tetris course
+
+    Input: ASM file with assembly language instructions
+    Output: input converted to machine code output
+    """
+
+    def __init__(self, input_file):
+        self.parser = HackAssemblerParser(input_file)
+        self.translator = HackAssemblerTranslator()
+
+    def run(self):
+        output_file = open('input_file name converted to .hack', 'w+')
+
+        with open(self.input_file) as asm_instructions:
+            for instruction in asm_instructions:
+                parsed_instruction = parser.parsed_instruction_for(instruction)
+                if parsed_instruction == "not valid asm instruction":
+                    continue
+                translated_instruction = translator.translate_instruction(parsed_instruction)
+                output_file.write(translated_instruction)
+
+
+class HackAssemblerTranslator():
+    def translated_line_for(self, instruction):
+
+
+class HackAssemblerParser():
+    """
+    Reads each line in the input file
+    """
     def __init__(self, input_file):
         self.input_file = input_file
+        self.current_line = None
 
-    def parse_file(self):
-        output_file = open('./add/Add.hack', 'w+')
-
-        for line in self.input_file:
-            # strip new line
-            line = line.rstrip('\n')
-            # skip if comment or empty line
-            if line == '' or line[0] == '/':
-                continue
-            # if A command
-            if line[0] == '@':
-                num_converted_to_binary_string = '{0:015}'.format(line[1::-1])
-                print(num_converted_to_binary_string)
-                output_file.write(num_converted_to_binary_string)
-
-            print(line)
-
-
-def main(input_file):
-    with open(input_file) as f:
-        parser = Parser(f)
-        output_file = parser.parse_file()
-
-    return output_file
+    def instruction_type(self, instruction):
+        # strip new line
+        instruction = instruction.rstrip('\n')
+        # skip if comment or empty line
+        if instruction == '' or instruction[0] == '/':
+            return "comment or empty line"
+        # if A command
+        if line[0] == '@':
+            return "A"
+            Assembler.translate_to_machine_code(line[1:], type='A')
+            num_converted_to_binary_string = '{0:016b}'.format(int(line[1:]))
+            output_file.write(num_converted_to_binary_string + '\n')
+        else: # C command
+            return "C"
 
 
-main(sys.argv[1])
+asm_input_file = sys.argv[1]
+assembler = HackAssembler(asm_input_file)
+assembler.run()
