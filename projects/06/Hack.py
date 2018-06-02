706a0b293c46ca3846c5db067beea9bdf412e244
Edward Dal Santo <edalsanto@doximity.com>
2018-06-02 14:22:13 -0700

    without symbols

--- projects/06/Hack.py
+++ projects/06/Hack.py
@@ -1,34 +1,7 @@
-# Input1: text file named SomeProgram.asm containing Hack assembly program
-# Input2: name of output file of SomeProgram.asm containing Hack assembly program
-# Output: text file name SomeProgram.hack containing translated Hack Machine Code
-
-# Assembler Without Symbols
-# main
-  # for each line in input file
-    # pass instruction to Parser
-      # Parse Module -> tokenize instruction components */
-        # note: ignore whitespaces and comments
-        # define helper functions which diagnose instruction
-        # main function logic */
-        # initialize binary string that will be written to output file
-        # if A command
-        #   add starting 0 for A command
-        #   set binary output string to decimal convert to binary
-        # else C command
-        #   Code Module */
-        #     define hash table
-        #       dest
-        #       comp
-        #         note: multiple keys with same value
-        #       jump
-        #   add starting 1 for C command
-        #   pass comp token to a-bit / comp-bits translator; add to binary output
-        #   pass dest-bits to translator; add to binary output
-        #   pass jump-bits to translator; add to binary output
-        # write binary string to output file
-
 #!/usr/local/bin/python
+
 import sys
+import re
 
 class HackAssembler():
     """
@@ -40,7 +13,7 @@ class HackAssembler():
 
     def __init__(self, input_file):
         self.parser = HackAssemblerParser(input_file)
-        self.translator = HackAssemblerTranslator()
+        self.translator = HackAssemblerDecoder()
 
     def run(self):
         hack_file_name = self.parser.input_file.name.split('.')[0] + '.hack'
@@ -48,39 +21,162 @@ class HackAssembler():
 
         while self.parser.has_more_commands():
             self.parser.advance()
-            translated_command = ''
+            machine_code_parts = []
 
             # comments and white space will be ignored
             if self.parser.command_type_is('a_command'):
-                num = int(self.parser.symbol())
-                print('num: {}'.format(num))
-                num_converted_to_binary_string = '{0:016b}'.format(num)
-                translated_command = num_converted_to_binary_string + '\n'
+                register_number = int(self.parser.symbol())
+                machine_code = HackAssemblerDecoder.decimal_to_binary_string(register_number)
+                machine_code_parts.append(machine_code)
             elif self.parser.command_type_is('l_command'):
                 continue
             elif self.parser.command_type_is('c_command'):
-                continue
+                # Init
+                machine_code_parts.append(HackAssemblerDecoder.C_COMMAND_INIT_BITS)
+                # Comp
+                comp_mnemonic = self.parser.comp_mnemonic()
+                comp_bits = HackAssemblerDecoder.COMP_MNEMONIC_TO_BITS[comp_mnemonic]
+                machine_code_parts.append(comp_bits)
+                # Dest
+                dest_mnemonic = self.parser.dest_mnemonic()
+                dest_bits = HackAssemblerDecoder.DEST_MNEMONIC_TO_BITS[dest_mnemonic]
+                machine_code_parts.append(dest_bits)
+                # Jump
+                jump_mnemonic = self.parser.jump_mnemonic()
+                jump_bits = HackAssemblerDecoder.JUMP_MNEMONIC_TO_BITS[jump_mnemonic]
+                machine_code_parts.append(jump_bits)
 
-            print('translated: {}'.format(translated_command))
-            hack_file.write(translated_command)
+            if len(machine_code_parts) > 0:
+                machine_code_command = ''.join(machine_code_parts)
+                hack_file.write(machine_code_command + '\n')
 
         hack_file.close()
 
 
-class HackAssemblerTranslator():
-    @classmethod
-    def dest_bits(cls, mnemonic):
-        print('dest')
+class HackAssemblerDecoder():
+    C_COMMAND_INIT_BITS = '111'
 
+    DEST_MNEMONIC_TO_BITS = {
+        None : '000',
+        'M'  : '001',
+        'D'  : '010',
+        'MD' : '011',
+        'A'  : '100',
+        'AM' : '101',
+        'AD' : '110',
+        'AMD': '111'
+    }
+
+    COMP_MNEMONIC_TO_BITS = {
+        None : '',
+        '0'  : '0101010',
+        '1'  : '0111111',
+        '-1' : '0111010',
+        'D'  : '0001100',
+        'A'  : '0110000',
+        'M'  : '1110000',
+        '!D' : '0001101',
+        '!A' : '0110011',
+        '!M' : '1110011',
+        'D+1': '0011111',
+        'A+1': '0110111',
+        'M+1': '1110111',
+        'D-1': '0001110',
+        'A-1': '0110010',
+        'M-1': '1110010',
+        'D+A': '0000010',
+        'D+M': '1000010',
+        'D-A': '0010011',
+        'D-M': '1010011',
+        'A-D': '0000111',
+        'M-D': '1000111',
+        'D&A': '0000000',
+        'D&M': '1000000',
+        'D|A': '0010101',
+        'D|M': '1010101'
+    }
+
+    JUMP_MNEMONIC_TO_BITS = {
+        None : '000',
+        'JGT': '001',
+        'JEQ': '010',
+        'JGE': '011',
+        'JLT': '100',
+        'JNE': '101',
+        'JLE': '110',
+        'JMP': '111'
+    }
+
+    @classmethod
+    def decimal_to_binary_string(cls, num):
+        return '{0:016b}'.format(num)
+
+
+class SymbolTable():
+    PREDEFINED_SYMBOLS = {
+        'SP': 0,
+        'LCL': 1,
+        'ARG': 2,
+        'THIS': 3,
+        'THAT': 4,
+        'R0': 0,
+        'R1': 1,
+        'R2': 2,
+        'R3': 3,
+        'R4': 4,
+        'R5': 5,
+        'R6': 6,
+        'R7': 7,
+        'R8': 8,
+        'R9': 9,
+        'R10': 10,
+        'R11': 11,
+        'R12': 12,
+        'R13': 13,
+        'R14': 14,
+        'R15': 15
+    }
+
+    def __init__(self):
+        self.symbols = PREDEFINED_SYMBOLS
+
+    def add_entry(self, symbol):
+        self.symbols[symbol] =
+
+    def contains(self, symbol):
+        return
+
+    def get_address(self, symbol):
+        return self.symbols[symbol]
 
 class HackAssemblerParser():
     """
     Reads each line in the input file one a time and reports on state of current line
     """
+    COMP_DELIMITER = '='
+    JUMP_DELIMITER = ';'
+
     def __init__(self, input_file):
         self.input_file = open(input_file, 'r')
         self.current_line = None
+        self.current_command = None
         self.next_line = None
+        self.valid_char_matcher = re.compile('[a-zA-Z0-9=+;]+')
+
+
+    def dest_mnemonic(self):
+        if self.current_line.find(self.JUMP_DELIMITER) != -1:
+            return self.current_line.split(self.JUMP_DELIMITER)[0]
+        elif self.current_line.find(self.COMP_DELIMITER) != -1:
+            return self.current_line.split(self.COMP_DELIMITER)[0]
+
+    def comp_mnemonic(self):
+        if self.current_line.find(self.COMP_DELIMITER) != -1:
+            return self.current_line.split(self.COMP_DELIMITER)[1].rstrip('\n')
+
+    def jump_mnemonic(self):
+        if self.current_line.find(self.JUMP_DELIMITER) != -1:
+            return self.current_line.split(self.JUMP_DELIMITER)[1].rstrip('\n')
 
     def symbol(self):
         """
@@ -101,26 +197,47 @@ class HackAssemblerParser():
         else:
             self.current_line = self.next_line
 
+        if self._matching_chars():
+            self.current_line = self.line_without_invalid_characters()
+
         self.next_line = self.input_file.readline()
+        self.find_new_command_type()
 
     def command_type_is(self, possible_command_type):
-        return possible_command_type == self.command_type()
+        return possible_command_type == self.current_command
 
     def command_type(self):
+        return self.command_type
+
+    def find_new_command_type(self):
         """
         assumes valid input
         """
         first_char = self.current_line[0]
 
-        # ignore white space and comments
-        if first_char == '/' or first_char == '\n':
-            return 'comment_or_empty_line'
+        if not self._valid_instruction():
+            self.current_command = 'comment_or_empty_line'
         elif first_char == '@':
-            return 'a_command'
+            self.current_command = 'a_command'
         elif first_char == '(':
-            return 'l_command'
+            self.current_command = 'l_command'
         else:
-            return 'c_command'
+            self.current_command = 'c_command'
+
+    def line_without_invalid_characters(self):
+        return self._matching_chars().group()
+
+    def _matching_chars(self):
+        return self.valid_char_matcher.match(self.current_line)
+
+    def _valid_instruction(self):
+        return not self._comment_line() and not self._empty_line()
+
+    def _comment_line(self):
+        return self.current_line[0] == '/'
+
+    def _empty_line(self):
+        return self.current_line == '\n'
 
 
 asm_input_file = sys.argv[1]
