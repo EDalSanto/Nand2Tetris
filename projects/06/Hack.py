56f7f7a269c24e85c8206b0c9ef8c73f28ec9fb3
Edward Dal Santo <edalsanto@doximity.com>
2018-06-02 07:52:12 -0700

    a command py

--- projects/06/Hack.py
+++ projects/06/Hack.py
@@ -43,19 +43,34 @@ class HackAssembler():
         self.translator = HackAssemblerTranslator()
 
     def run(self):
-        output_file = open('input_file name converted to .hack', 'w+')
+        hack_file_name = self.parser.input_file.name.split('.')[0] + '.hack'
+        hack_file = open(hack_file_name, 'w+')
 
         while self.parser.has_more_commands():
+            self.parser.advance()
+            translated_command = ''
 
+            # comments and white space will be ignored
+            if self.parser.command_type_is('a_command'):
+                num = int(self.parser.symbol())
+                print('num: {}'.format(num))
+                num_converted_to_binary_string = '{0:016b}'.format(num)
+                translated_command = num_converted_to_binary_string + '\n'
+            elif self.parser.command_type_is('l_command'):
+                continue
+            elif self.parser.command_type_is('c_command'):
+                continue
 
-        if parsed_instruction == "not valid asm instruction":
-            continue
-        translated_instruction = translator.translate_instruction(parsed_instruction)
-        output_file.write(translated_instruction)
+            print('translated: {}'.format(translated_command))
+            hack_file.write(translated_command)
+
+        hack_file.close()
 
 
 class HackAssemblerTranslator():
-    def translated_line_for(self, instruction):
+    @classmethod
+    def dest_bits(cls, mnemonic):
+        print('dest')
 
 
 class HackAssemblerParser():
@@ -65,24 +80,47 @@ class HackAssemblerParser():
     def __init__(self, input_file):
         self.input_file = open(input_file, 'r')
         self.current_line = None
+        self.next_line = None
+
+    def symbol(self):
+        """
+        remove symbols for A or Commands + trailing comments
+        """
+        return ''.join(c for c in self.current_line if c not in '()@/')
+
+    def has_more_commands(self):
+        # if empty line, would at least return \n
+        return self.next_line != ''
 
     def advance(self):
-        self.current_line = self.input_file.readline()
+        """
+        get next line as well so we know if there are more lines after the current
+        """
+        if self.current_line == None:
+            self.current_line = self.input_file.readline()
+        else:
+            self.current_line = self.next_line
 
-    def instruction_type(self, instruction):
-        # strip new line
-        instruction = instruction.rstrip('\n')
-        # skip if comment or empty line
-        if instruction == '' or instruction[0] == '/':
-            return "comment or empty line"
-        # if A command
-        if line[0] == '@':
-            return "A"
-            Assembler.translate_to_machine_code(line[1:], type='A')
-            num_converted_to_binary_string = '{0:016b}'.format(int(line[1:]))
-            output_file.write(num_converted_to_binary_string + '\n')
-        else: # C command
-            return "C"
+        self.next_line = self.input_file.readline()
+
+    def command_type_is(self, possible_command_type):
+        return possible_command_type == self.command_type()
+
+    def command_type(self):
+        """
+        assumes valid input
+        """
+        first_char = self.current_line[0]
+
+        # ignore white space and comments
+        if first_char == '/' or first_char == '\n':
+            return 'comment_or_empty_line'
+        elif first_char == '@':
+            return 'a_command'
+        elif first_char == '(':
+            return 'l_command'
+        else:
+            return 'c_command'
 
 
 asm_input_file = sys.argv[1]
