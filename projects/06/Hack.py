bc6f59a996f7efd4ea89a7ab6706d9f76dba39c9
Edward Dal Santo <edalsanto@doximity.com>
2018-06-04 17:04:23 +0900

    fix add entry

--- projects/06/Hack.py
+++ projects/06/Hack.py
@@ -44,8 +44,9 @@ class HackAssembler():
 
             if self.parser.command_type_is('a_command'):
                 symbol = self.parser.symbol()
+                not_number = char_matcher.match(symbol)
 
-                if char_matcher.match(symbol):
+                if not_number:
                     if self.symbol_table.contains(symbol):
                         register_number = self.symbol_table.get_address(symbol)
                     else:
@@ -144,40 +145,43 @@ class HackAssemblerDecoder():
 
 class SymbolTable():
     PREDEFINED_SYMBOLS = {
-        'SP': 0,
-        'LCL': 1,
-        'ARG': 2,
+        'SP'  : 0,
+        'LCL' : 1,
+        'ARG' : 2,
         'THIS': 3,
         'THAT': 4,
-        'R0': 0,
-        'R1': 1,
-        'R2': 2,
-        'R3': 3,
-        'R4': 4,
-        'R5': 5,
-        'R6': 6,
-        'R7': 7,
-        'R8': 8,
-        'R9': 9,
-        'R10': 10,
-        'R11': 11,
-        'R12': 12,
-        'R13': 13,
-        'R14': 14,
-        'R15': 15
+        'R0'  : 0,
+        'R1'  : 1,
+        'R2'  : 2,
+        'R3'  : 3,
+        'R4'  : 4,
+        'R5'  : 5,
+        'R6'  : 6,
+        'R7'  : 7,
+        'R8'  : 8,
+        'R9'  : 9,
+        'R10' : 10,
+        'R11' : 11,
+        'R12' : 12,
+        'R13' : 13,
+        'R14' : 14,
+        'R15' : 15,
+        'SCREEN': 16384,
+        'KBD'   : 24576
     }
 
     def __init__(self):
         self.symbols = self.PREDEFINED_SYMBOLS
+        self.next_available_memory_address = 16
 
     def add_entry(self, symbol=None, address=None):
         if address:
             self.symbols[symbol] = address
         else:
-            self.symbols[symbol] = len(self.symbols)
+            self.symbols[symbol] = self.next_available_memory_address
+            self.next_available_memory_address += 1
 
         return self.get_address(symbol)
-
 
     def contains(self, symbol):
         return symbol in self.symbols
