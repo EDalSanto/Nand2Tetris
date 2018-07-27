efd3363421005d22893030eb17573afbcb4b62bc
Edward Dal Santo <ejds001@gmail.com>
2018-07-26 17:31:16 -0700

    setup advance test

--- projects/10/test/jack_tokenizer.py
+++ projects/10/test/jack_tokenizer.py
@@ -1,16 +1,29 @@
 import unittest
-from JackTokenizer import JackTokenizer
+
+# add source files to path
+import os, sys
+sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
+
+from source.JackTokenizer import JackTokenizer
 
 class TestJackTokenizer(unittest.TestCase):
-    input = """\
-            if (x < 0) {
-                let state = "negative";
-            }
-            """
+    source_code = """\
+                  if (x < 0) {
+                      let state = "negative";
+                  }
+                  """
 
     def test_advance(self):
-        # it gets the next token
-        self.assertEqual('foo', input)
+        tokenizer = JackTokenizer(self.source_code)
+        # it gets the first token
+        tokenizer.advance()
+        self.assertEqual('if', tokenizer.current_token)
+        # it gets the second token
+        tokenizer.advance()
+        self.assertEqual('(', tokenizer.current_token)
+        # it gets the thirdtoken
+        tokenizer.advance()
+        self.assertEqual('(', tokenizer.current_token)
 
 if __name__ == "__main__":
     unittest.main()
