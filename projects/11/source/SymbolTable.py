65b2d40db62e01b0849b3953a1335289210271a9
Edward Dal Santo <ejds001@gmail.com>
2018-08-10 21:26:32 -0700

    work on symbol table

--- projects/11/source/SymbolTable.py
+++ projects/11/source/SymbolTable.py
@@ -2,38 +2,55 @@ class SymbolTable():
     """
     Represents the current symbols for a given class or subroutine for the JackCompiler
     """
+    CLASS_SCOPED_VARIABLES = [ 'field', 'static' ]
 
     def __init__(self):
-        self.symbols = {}
+        self.symbols = []
 
-    def start_subroutine(self):
+    def reset(self):
         """
         Starts a new subroutine scope, i.e., resets the subroutine's scope
         """
-        self.symbols = {}
+        self.symbols = []
 
     def define(self, name, symbol_type, kind):
         """
         defines a new identifier of the given name, type, and kind and assigns it a running index
         """
+        new_symbol = {
+            'name': name,
+            'type': symbol_type,
+            'kind': kind,
+            'index': self.var_count(kind)
+        }
+        self.symbols.append(new_symbol)
 
     def var_count(self, kind):
         """
         returns the number of variables of the given kind already defined in the current scope
         """
+        return sum(symbol['kind'] == kind for symbol in self.symbols)
 
     def kind_of(self, name):
         """
         returns the kind of the named identifer in the current scope
         (STATIC, FIELD, ARG, VAR, NONE)
         """
+        return self._find_symbol_by_name(name).get('kind')
 
     def type_of(self, name):
         """
         returns the type of the named identifier in the current scope
         """
+        return self._find_symbol_by_name(name).get('type')
 
     def index_of(self, name):
         """
         returns the index assigned to the named identifier
         """
+        return self._find_symbol_by_name(name).get('index')
+
+    def _find_symbol_by_name(self, value)
+        for symbol in self.symbols:
+            if symbol['name'] == value:
+                return symbol
